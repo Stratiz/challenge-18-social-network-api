f9d17fd6bb785b2b7eddd0f4cf813693f806900e
Stratiz <36082806+Stratiz@users.noreply.github.com>
2022-09-04 04:35:37 -0500

    new: Made models

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    }
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 15,
+      maxLength: 500,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    reactions: [reactionSchema]
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
+// Schema to create Post model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      default: "",
+      unqiue: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      default: "",
+      unqiue: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ]
+  }
+);
+
+// Create a virtual property `tagCount` that gets the amount of comments per user
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our Post model
+const User = model('user', userSchema);
+
+module.exports = User;
